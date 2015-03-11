2db7dea5ee3c7b424a05c5ebb1370d15216e601f
Wojciech Gaca <wmgaca@gmail.com>
2015-03-11 11:07:19 +0400

    This and that.

---
+++ src/myio.py
@@ -0,0 +1,42 @@
+import sys
+
+
+class Color(object):
+
+    RED = 31
+    GREEN = 32
+    YELLOW = 33
+    BLUE = 34
+
+
+def _print(*messages, **kwargs):
+    if not messages:
+        print ''
+        return
+
+    elif len(messages) == 1:
+        messages = messages[0].split('\n')
+
+    color = kwargs.get('color', None)
+
+    for message in messages:
+        if color:
+            print "\033[00;%sm%s\033[0m" % (color, message)
+        else:
+            print message
+
+
+def success(*messages):
+    _print(*messages, color=Color.GREEN)
+
+
+def error(*messages, **kwargs):
+    _print(*messages, color=Color.RED)
+
+
+def info(*messages):
+    _print(*messages, color=Color.BLUE)
+
+
+def echo(*messages):
+    _print(*messages)
